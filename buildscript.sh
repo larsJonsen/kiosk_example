26037f6743ccd83d6c3bb2e5ad9d61871c81a1bc
Lars Ravn-Jonsen <lars.jonsen@gmail.com>
2025-08-09 19:16:54 +0200

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export MIX_ENV=prod
+
+export MIX_ENV=dev
 export MIX_TARGET=rpi4
 export ERL_COMPILER_OPTIONS="[{d,'SKIP_NIF_LOADING'}]"
 
 mix clean
-mix deps.clean --build
+mix deps.clean --all
 mix deps.get
-mix compile
 mix firmware
